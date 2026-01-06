3b4a829d05037274d5e779bb037e2a897aa95820
Oleh Skibinskyi <oleg.sk2002@gmail.com>
2026-01-06 18:48:24 +0200

    resolved flake8 problem

--- user/serializers.py
+++ user/serializers.py
@@ -48,7 +48,9 @@ class AuthTokenSerializer(serializers.Serializer):
 
         if email and password:
             user = authenticate(
-                request=self.context.get("request"), username=email, password=password
+                request=self.context.get("request"),
+                username=email,
+                password=password
             )
             if not user:
                 msg = _("Unable to log in with provided credentials.")
